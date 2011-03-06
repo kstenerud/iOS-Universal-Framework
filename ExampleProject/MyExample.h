2953e91d949ea9971a773576d7052837e0191369
Karl Stenerud <kstenerud@gmail.com>
2011-03-06 08:26:00 -0800

    Initial commit

---
+++ ExampleProject/MyExample.h
@@ -0,0 +1,18 @@
+//
+//  MyExample.h
+//  ExampleProject
+//
+//  Created by Karl Stenerud on 3/4/11.
+//  Copyright 2011 xxx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MyExample : NSObject {
+
+}
+
++ (void) doSomething;
+
+@end
---
+++ ExampleProject/MyExample.m
@@ -0,0 +1,19 @@
+//
+//  MyExample.m
+//  ExampleProject
+//
+//  Created by Karl Stenerud on 3/4/11.
+//  Copyright 2011 xxx. All rights reserved.
+//
+
+#import "MyExample.h"
+
+
+@implementation MyExample
+
++ (void) doSomething
+{
+	NSLog(@"Doing something!");
+}
+
+@end
